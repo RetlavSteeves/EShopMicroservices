d4e6e43fc1ccfd8955a5ca8979263f35d801ba87
RetlavSteeves <valterhugo.esteves@gmail.com>
2024-10-08 11:57:44 +0100

    Create Product endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,28 @@
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreatePRoductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", 
+            async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreatePRoductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+
+        })
+        .WithName("CreateProduct")
+        .Produces<CreatePRoductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
+}
